52d23d69647f452dd56f1fa0b748e2d1d92aac8d
Mauricio Arrieira <mauricio.arrieira@pampa.compasso>
2021-06-02 16:45:03 -0300

    editar e remover lancamentos

--- src/components/Movimentos.vue
+++ src/components/Movimentos.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="container">
     <div class="box">
-      <v-card>
+      <v-card v-if="!isEditing">
         <v-card-title class="custom-search-field">
           <v-text-field
             v-model="search"
             append-icon="mdi-magnify"
             label="Pesquisar"
             single-line
-            hide-details
+            outlined
           ></v-text-field>
         </v-card-title>
         <v-data-table
@@ -20,12 +20,43 @@
           @click:row="handleClick"
         ></v-data-table>
       </v-card>
+      <div v-else>
+        <v-row>
+          <v-col>
+            <label>Categoria: {{ movimento.categoria }}</label>
+          </v-col>
+          <v-col>
+            <label>Valor: </label>
+            <money v-model="movimento.valor" v-bind="money"></money>
+          </v-col>
+        </v-row>
+        <div style="margin-top: 15px">
+          <label>Descrição: </label>
+          <textarea v-model="movimento.movimento"></textarea>
+          <button class="btnSave" v-on:click="saveLancamento()">
+            <i class="fa fa-check-circle"></i>Salvar
+          </button>
+          <v-snackbar v-model="snackbar">
+            {{ message }}
+
+            <template v-slot:action="{ attrs }">
+              <v-btn color="blue" text v-bind="attrs" @click="snackbar = false">
+                X
+              </v-btn>
+            </template>
+          </v-snackbar>
+          <button class="btnSave" v-on:click="deleteLancamento()">
+            <i class="fas fa-trash"></i>Excluir
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 const axios = require("axios");
+import { Money } from "v-money";
 
 export default {
   name: "Movimentos",
@@ -42,7 +73,19 @@ export default {
         { text: "Valor (R$)", value: "valor", class: "headers" },
         { text: "Data", value: "data", class: "headers" },
       ],
+      money: {
+        decimal: ",",
+        thousands: ".",
+        prefix: "R$ ",
+        suffix: "",
+        precision: 2,
+        masked: false,
+      },
       movimentos: [],
+      movimento: null,
+      isEditing: false,
+      snackbar: false,
+      message: "",
     };
   },
   mounted() {
@@ -51,14 +94,45 @@ export default {
       this.formatValores();
     });
   },
+  components: { Money },
   methods: {
     handleClick(value) {
       console.log("value: ", value);
+      this.movimento = value;
+      this.isEditing = true;
     },
     formatValores() {
       this.movimentos.forEach((movimento) => {
         movimento.valor = movimento.valor.toString().replace(".", ",");
       });
+    },
+    saveLancamento() {
+      this.movimento.data = this.buildFormattedDate();
+      axios
+        .post("http://localhost:8081/salvar", this.movimento)
+        .then((response) => {
+          if (response.data !== null) {
+            this.message = "Lançamento editado com sucesso!";
+            this.snackbar = true;
+            setTimeout(() => (this.isEditing = false), 3000);
+          }
+        });
+    },
+    deleteLancamento() {
+      axios
+        .post("http://localhost:8081/excluir/" + this.movimento.id)
+        .then((response) => {
+          this.movimentos = response.data;
+          this.message = "Lançamento excluido com sucesso!";
+          this.snackbar = true;
+          setTimeout(() => (this.isEditing = false), 3000);
+        });
+    },
+    buildFormattedDate() {
+      let date = new Date().toISOString();
+      date = date.split("T")[0].split("-");
+      const formattedDate = date[2] + "/" + date[1] + "/" + date[0];
+      return formattedDate;
     },
   },
 };
@@ -74,9 +148,11 @@ export default {
 }
 i {
   color: white !important;
+  font-size: 18px;
 }
 .container {
   padding: 20px;
+  padding-top: 30px !important;
 }
 .box {
   padding: 10px;
@@ -93,5 +169,44 @@ i {
 .custom-search-field {
   width: 30% !important;
   font-family: Arial, Helvetica, sans-serif !important;
+}
+textarea {
+  border: 1px solid #2c3e50;
+  border-radius: 2px;
+  width: 100%;
+  padding: 10px;
+  resize: none;
+  margin-top: 10px;
+}
+input {
+  border-bottom: 1px solid #2c3e50;
+  margin-top: 10px;
+  width: 100%;
+}
+input:focus {
+  outline: none;
+}
+button {
+  background-color: gray;
+  color: white;
+  padding: 15px;
+  border-radius: 2px;
+  margin-right: 10px;
+}
+
+button:hover {
+  background-color: white;
+  color: #2c3e50;
+  border: 1px solid #2c3e50;
+}
+.btnSave {
+  margin-top: 20px;
+  background-color: #2c3e50;
+}
+
+.btnSave:hover {
+  background-color: #2c3e50;
+  color: white;
+  border: 1px solid gray;
 }
 </style>
